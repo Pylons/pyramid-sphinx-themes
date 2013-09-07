6e2bad10b64a9206aa55432f06c5baf82c5d14d8
Blaise Laflamme <blaise@laflamme.org>
2013-09-07 15:25:27 -0400

    added generated grunt files

---
+++ src/pyramid_sphinx_themes/pyramid/static/dist/js/theme.js
@@ -0,0 +1,15 @@
+/*!
+* pyramid-sphinx-themes - v0.1.0
+* http://pylonsproject.org/
+* 2013 
+*/
+
+!function ($) {
+
+  "use strict";
+
+  $(document).ready(function() {
+    // Do domething
+  });
+
+}(window.jQuery);

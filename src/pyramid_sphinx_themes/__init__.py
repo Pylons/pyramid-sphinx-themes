e5c5a0c64cad73df0dc65308451575ff16651705
Blaise Laflamme <blaise@laflamme.org>
2013-09-07 20:10:21 -0400

    monkey patched sphinx html builder to override js files

--- src/pyramid_sphinx_themes/__init__.py
+++ src/pyramid_sphinx_themes/__init__.py
@@ -2,6 +2,22 @@
 
 import os
 
+from sphinx.builders.html import StandaloneHTMLBuilder
+
+
+def patch_sphinx():
+    # Override files... Why we could not!!
+    script_files = [
+        '_static/dist/js/jquery.min.js',
+        '_static/dist/js/underscore-min.js',
+        '_static/dist/js/bootstrap.min.js',
+        '_static/dist/js/html5shiv.js',
+        '_static/dist/js/respond.min.js',
+        '_static/dist/js/doctools.js',
+        '_static/dist/js/theme.js'
+        ]
+    StandaloneHTMLBuilder.script_files = script_files
+
 
 def get_html_themes_path():
     """Return list of sphinx themes."""

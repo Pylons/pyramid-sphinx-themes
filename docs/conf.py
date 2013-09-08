067514fe90fca327f4d410145e824e01c8fcb512
Blaise Laflamme <blaise@laflamme.org>
2013-09-07 23:29:05 -0400

    renamed pyramid theme for ground to extends from

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,7 @@ patch_sphinx()
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pyramid'
+html_theme = 'ground'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
